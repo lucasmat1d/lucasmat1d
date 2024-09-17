d7fc7eae0b7aa9fbd608fccb5edeec71ebd8e080
lucasmat1d <eucontomeianoite195@gmail.com>
2024-09-17 19:59:12 -0300

    adicionando arquivo js e css

---
+++ style.css
@@ -0,0 +1,54 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+  }function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+      rect(0, 0, 100, 150)
+  }function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+  
+      fill("red")
+      rect(0, 0, 100, 150)
+  }function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+  
+      fill("red")
+      rect(0, 0, 100, 150)
+  }function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+  
+      stroke("blue")
+      fill("red")
+      rect(0, 0, 100, 150)
+  }function s
